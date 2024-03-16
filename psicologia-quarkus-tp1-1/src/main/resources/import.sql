c43f97f327dceed497fd6faae6e2212452fe2485
Alex-Trauthman <122585413+Alex-Trauthman@users.noreply.github.com>
2024-03-15 22:28:10 -0300

    Delete psicologia-quarkus-tp1-1 directory

--- psicologia-quarkus-tp1-1/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT INTO produto (descricao, marca, valor) VALUES ('ração de cachorro 1kg', 'El Copir', 32.90);
-INSERT INTO produto (descricao, marca, valor) VALUES ('ração para filhotes, cachorros e gatos 3 kg', 'GATHRRGOS', 29.00);
